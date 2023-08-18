6c993b20bd85b4dfeb3471810ea60a3dfdd4bc77
K@RT#!_B#@! <108513650+Karthi-bhai@users.noreply.github.com>
2023-08-18 15:16:35 +0530

    Add files via upload

---
+++ Day1b/src/main/java/com/example/demo/controllers/ApiController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+	
+	@Value("${var}")
+	private String studentName;
+	@GetMapping("/")
+	public String getName()
+	{
+		return "Welcome "+studentName+"!";
+	}
+
+}
